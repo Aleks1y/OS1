de09de9b7e4850d12820c49dd6ef8b661081a296
Aleks1y <74914340+Aleks1y@users.noreply.github.com>
2021-09-29 22:24:31 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+void print_text(char* scr) {
+  for(int i = 0; i < 10; i++)	{
+    printf("%s %d\n", str, i);
+  }
+}
+
+void *thread_body(void *param) { 
+  print_texc("Child");
+  return NULL;
+}
+
+int main(int argc, char *argv[]) { 
+  pthread_c thread; 
+  int code;
+  
+  code = pthread_create(Sthread, NULL, thread_body, NULL); 
+  if (code!=0)	{
+    char buf[256];
+    strerror_r(code, buf, sizeof buf);
+    fprintf(stderr, "%s: creating thread error: %s\n", argv[0], buf); 
+    exit(1);
+  }
+  
+  print_text("Parent"); 
+  pthread exit(NULL);
+}
