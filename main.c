1dde669e6abd621334cbc217ea820bfd4a5713b2
Aleks1y <74914340+Aleks1y@users.noreply.github.com>
2021-10-14 11:32:59 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <string.h>
 
 #define BUF_SIZE 256
 #define SUCCESS 0
@@ -24,12 +23,7 @@ int main(int argc, char *argv[]) {
   
   code = pthread_create(&thread, NULL, thread_body, NULL); 
   if (code! = SUCCESS)	{
-    char* buf = malloc(BUF_SIZE);
-    while(ERANGE == strerror_r(code, buf, sizeof buf)){
-      buf = realloc(buf, sizeof(buf) * 2);
-    }
-    fprintf(stderr, "%s: creating thread error: %s\n", argv[0], buf);
-    free(buf);
+    perror("creating thread error");
     exit(ERROR);
   }
   
